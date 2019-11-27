57331210ef91b0ea006c2ecc56d26fc21e66d736
Zodsmar <zodsmar@gmail.com>
2019-11-27 14:29:17 -0500

    Formatting I am happier with using MiGLayout

--- src/main/java/sassa/gui/GenerateGUI.java
+++ src/main/java/sassa/gui/GenerateGUI.java
@@ -1,6 +1,7 @@
 package sassa.gui;
 
 import com.jgoodies.forms.layout.*;
+import net.miginfocom.swing.MigLayout;
 import org.json.simple.parser.ParseException;
 import sassa.main.Main;
 import sassa.util.Util;
@@ -24,29 +25,40 @@ public class GenerateGUI {
         genGUI.getContentPane().add(panel_generated);
         panel_generated.setLayout(new BorderLayout(0, 0));
 
+        panel_generated.setPreferredSize(new Dimension(700, 515));
+
         JScrollPane generatedScrollBar = new JScrollPane();
         generatedScrollBar.setBounds(0, 33, Main.FRAME_SCROLL_BAR_WIDTH, Main.FRAME_SCROLL_BAR_HEIGHT);
         generatedScrollBar.getVerticalScrollBar().setUnitIncrement(10);
         panel_generated.add(generatedScrollBar);
 
         JPanel biomesPanel = new JPanel();
-
         Util gen = new Util();
         ArrayList<String> searchingList = gen.createSearchLists("Biomes");
 
-        biomesPanel.setLayout(new GridLayout(searchingList.size()/3, 3));
+        biomesPanel.setLayout(new MigLayout("center center, wrap 3", // layout
+               " [center]40", // column
+                "[center]40" // row
+                 ));
 
         generatedScrollBar.setViewportView(biomesPanel);
 
         for(int i = 0; i < searchingList.size(); i++){
                 Container container = new Container();
-                Border border = BorderFactory.createLineBorder(Color.darkGray, 1);
+                //Border border = BorderFactory.createLineBorder(Color.darkGray, 2);
+
+                int eb = 10;
                 JLabel label = new JLabel(searchingList.get(i));
-                label.setBorder(border);
-                label.setHorizontalAlignment(JLabel.CENTER);
+                label.setBorder(BorderFactory.createCompoundBorder(
+                        BorderFactory.createLineBorder(Color.darkGray, 2),
+                        BorderFactory.createEmptyBorder(eb, eb, eb, eb)
+                       ));
+
                 container.add(label);
                 container.setLayout(new BoxLayout(container, BoxLayout.PAGE_AXIS));
                 biomesPanel.add(container);
+
+
         }
         genGUI.setLocationRelativeTo(null);
         genGUI.pack();
