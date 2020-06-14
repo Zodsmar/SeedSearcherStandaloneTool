e850750df8ff8c06c1eb0234c15700f985530f23
Zodsmar <zodsmar@gmail.com>
2020-06-14 14:35:33 -0400

    BiomeUtils in and working next is to build it up for speed

--- src/main/java/sassa/main/mainApp.java
+++ src/main/java/sassa/main/mainApp.java
@@ -7,11 +7,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import kaptainwutax.biomeutils.Biome;
+import kaptainwutax.biomeutils.source.OverworldBiomeSource;
+import kaptainwutax.seedutils.mc.MCVersion;
 import org.json.simple.parser.ParseException;
 import sassa.gui.fxmlController;
 import sassa.util.Version;
 
 import java.io.IOException;
+import java.util.Random;
 
 public class mainApp extends Application {
 
@@ -33,6 +37,14 @@ public class mainApp extends Application {
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(Main.class.getResource("/sassa/fxml/layout.fxml"));
         BorderPane vbox = loader.load();
+        Random test = new Random();
+        long seed = test.nextLong();
+
+        // get biomes via BiomeUtils
+//        OverworldBiomeSource overworld = new OverworldBiomeSource(MCVersion.v1_15, 1);
+//        overworld.build();
+//        System.out.println(overworld.getBiome(0,0,0) == Biome.OCEAN);
+
 
         Scene scene = new Scene(vbox);
         mainStage.setTitle("Sassa-" + VERSION);
