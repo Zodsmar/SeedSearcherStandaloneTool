1465bb9eeedc7626450b9f286ec7d1daa2a12754
Matěj Račinský <racinsky.matej@seznam.cz>
2020-08-26 22:37:22 +0200

    raised minimal minecraft version for structures

--- src/main/java/sassa/util/Structures.java
+++ src/main/java/sassa/util/Structures.java
@@ -12,8 +12,8 @@ public class Structures {
 
     static {
         //TODO: Mineshafts and Strongholds
-        STRUCTURE.put(Structure.getName(Village.class), new StructureProvider(Village::new, MCVersion.v1_7, "OVERWORLD", 1));
-        STRUCTURE.put(Structure.getName(SwampHut.class), new StructureProvider(SwampHut::new, MCVersion.v1_7, "OVERWORLD", 1));
+        STRUCTURE.put(Structure.getName(Village.class), new StructureProvider(Village::new, MCVersion.v1_8, "OVERWORLD", 1));
+        STRUCTURE.put(Structure.getName(SwampHut.class), new StructureProvider(SwampHut::new, MCVersion.v1_8, "OVERWORLD", 1));
         STRUCTURE.put(Structure.getName(Shipwreck.class), new StructureProvider(Shipwreck::new, MCVersion.v1_13, "OVERWORLD", 1));
         STRUCTURE.put(Structure.getName(RuinedPortal.class) + "_overworld", new StructureProvider(RuinedPortal::new, MCVersion.v1_16, "OVERWORLD", 1));
         STRUCTURE.put(Structure.getName(RuinedPortal.class) + "_nether", new StructureProvider(RuinedPortal::new, MCVersion.v1_16, "NETHER", 1));
@@ -22,11 +22,11 @@ public class Structures {
         STRUCTURE.put(Structure.getName(NetherFossil.class), new StructureProvider(NetherFossil::new, MCVersion.v1_16, "NETHER", 1));
         STRUCTURE.put(Structure.getName(Monument.class), new StructureProvider(Monument::new, MCVersion.v1_8, "OVERWORLD", 1));
         STRUCTURE.put(Structure.getName(Mansion.class), new StructureProvider(Mansion::new, MCVersion.v1_11, "OVERWORLD", 1));
-        STRUCTURE.put(Structure.getName(JunglePyramid.class), new StructureProvider(JunglePyramid::new, MCVersion.v1_7, "OVERWORLD", 1));
+        STRUCTURE.put(Structure.getName(JunglePyramid.class), new StructureProvider(JunglePyramid::new, MCVersion.v1_8, "OVERWORLD", 1));
         STRUCTURE.put(Structure.getName(Igloo.class), new StructureProvider(Igloo::new, MCVersion.v1_9, "OVERWORLD", 1));
-        STRUCTURE.put(Structure.getName(Fortress.class), new StructureProvider(Fortress::new, MCVersion.v1_7, "NETHER", 1));
-        STRUCTURE.put(Structure.getName(EndCity.class), new StructureProvider(EndCity::new, MCVersion.v1_7, "END", 1));
-        STRUCTURE.put(Structure.getName(DesertPyramid.class), new StructureProvider(DesertPyramid::new, MCVersion.v1_7, "OVERWORLD", 1));
+        STRUCTURE.put(Structure.getName(Fortress.class), new StructureProvider(Fortress::new, MCVersion.v1_8, "NETHER", 1));
+        STRUCTURE.put(Structure.getName(EndCity.class), new StructureProvider(EndCity::new, MCVersion.v1_8, "END", 1));
+        STRUCTURE.put(Structure.getName(DesertPyramid.class), new StructureProvider(DesertPyramid::new, MCVersion.v1_8, "OVERWORLD", 1));
         STRUCTURE.put(Structure.getName(BuriedTreasure.class), new StructureProvider(BuriedTreasure::new, MCVersion.v1_13, "OVERWORLD", 1));
         STRUCTURE.put(Structure.getName(BastionRemnant.class), new StructureProvider(BastionRemnant::new, MCVersion.v1_16, "NETHER", 1));
     }
