3ad6f94efb4f3e40af18220b0ef3c70e92d14be6
Zodsmar <zodsmar@gmail.com>
2021-11-19 00:18:37 -0500

    Fixed a few bugs with the searcher. It actually fully works now

--- src/main/java/sassa/searcher/Searching_Thread.java
+++ src/main/java/sassa/searcher/Searching_Thread.java
@@ -80,100 +80,94 @@ public class Searching_Thread extends Thread implements Runnable {
 
         // For now this will loop within the range to make sure we stay within the range for this thread
         for (long structureSeedIncrementer = startFeatureSeed; structureSeedIncrementer < endFeatureSeed; structureSeedIncrementer++) {
+            start:
+            {
+                //TODO This call can have duplicates, ideally we want to get the next random seed and make sure its not a duplicate
+                // tried above but the call is way to expensive need to look back into this.
+                long structureSeed = threadLocalRandomizer.nextLong(startFeatureSeed, endFeatureSeed);
 
-            //TODO This call can have duplicates, ideally we want to get the next random seed and make sure its not a duplicate
-            // tried above but the call is way to expensive need to look back into this.
-            long structureSeed = threadLocalRandomizer.nextLong(startFeatureSeed, endFeatureSeed);
+                //Do the feature search and if the features we wanted exists it will return true along with all the chunk positions
+                // if it returns false then the remaining code is passed, and it will start a new seed
+                HashMap<Boolean, HashMap<Feature, List<CPos>>> checkingFeatures = featureSearch(featureList, structureSeed, rand);
+                boolean shouldContinue = checkingFeatures.containsKey(true);
 
-            //Do the feature search and if the features we wanted exists it will return true along with all the chunk positions
-            // if it returns false then the remaining code is passed, and it will start a new seed
-            HashMap<Boolean, HashMap<Feature, List<CPos>>> checkingFeatures = featureSearch(featureList, structureSeed, rand);
-            boolean shouldContinue = checkingFeatures.containsKey(true);
+                //Validate that all the structures we want to spawn are possible first or if no structures are wanted just continue to biomes
+                if (shouldContinue || featureList.size() == 0) {
+                    Map<Feature, List<CPos>> foundFeatures = checkingFeatures.get(true);
+                    //Now we convert the structure seed to a world seed and check the biomes
+                    for (long upperBits = 0; upperBits < 1L << model.getBiomePrecision(); upperBits++) {
+                        long worldSeed = (upperBits << 48) | structureSeed;
 
-            //Validate that all the structures we want to spawn are possible first or if no structures are wanted just continue to biomes
-            if (shouldContinue || featureList.size() == 0) {
-                Map<Feature, List<CPos>> foundFeatures = checkingFeatures.get(true);
-                //Now we convert the structure seed to a world seed and check the biomes
-                for (long upperBits = 0; upperBits < 1L << model.getBiomePrecision(); upperBits++) {
-                    long worldSeed = (upperBits << 48) | structureSeed;
+                        //At this point we can now get the spawnpoint and world types
+                        GenerationContext.Context contextO = GenerationContext.getContext(worldSeed, Dimension.OVERWORLD, model.getSelectedVersion());
+                        OverworldBiomeSource sourceO = (OverworldBiomeSource) contextO.getBiomeSource();
+                        if (model.getWorldType() == WorldType.LARGE_BIOMES) {
+                            sourceO.biomeSize = 6;
+                        }
 
-                    //At this point we can now get the spawnpoint and world types
-                    GenerationContext.Context contextO = GenerationContext.getContext(worldSeed, Dimension.OVERWORLD, model.getSelectedVersion());
-                    OverworldBiomeSource sourceO = (OverworldBiomeSource) contextO.getBiomeSource();
-                    if (model.getWorldType() == WorldType.LARGE_BIOMES) {
-                        sourceO.biomeSize = 6;
-                    }
+                        GenerationContext.Context contextN = GenerationContext.getContext(worldSeed, Dimension.NETHER, model.getSelectedVersion());
+                        NetherBiomeSource sourceN = (NetherBiomeSource) contextN.getBiomeSource();
 
-                    GenerationContext.Context contextN = GenerationContext.getContext(worldSeed, Dimension.NETHER, model.getSelectedVersion());
-                    NetherBiomeSource sourceN = (NetherBiomeSource) contextN.getBiomeSource();
+                        GenerationContext.Context contextE = GenerationContext.getContext(worldSeed, Dimension.END, model.getSelectedVersion());
+                        EndBiomeSource sourceE = (EndBiomeSource) contextE.getBiomeSource();
 
-                    GenerationContext.Context contextE = GenerationContext.getContext(worldSeed, Dimension.END, model.getSelectedVersion());
-                    EndBiomeSource sourceE = (EndBiomeSource) contextE.getBiomeSource();
+                        ///////////// SpawnPoint Checking ////////////////
 
-                    ///////////// SpawnPoint Checking ////////////////
+                        int spawnPointX;
+                        int spawnPointZ;
 
-                    int spawnPointX;
-                    int spawnPointZ;
-
-                    switch (model.getSpawnType()) {
-                        case APPROXIMATE:
-                            BPos spawnPoint = SpawnPoint.getApproximateSpawn(sourceO);
-                            spawnPointX = spawnPoint.getX();
-                            spawnPointZ = spawnPoint.getZ();
-                            break;
-                        case TRUESPAWN:
-                            OverworldTerrainGenerator ts = new OverworldTerrainGenerator(sourceO);
-                            BPos spawnPointT = SpawnPoint.getSpawn(ts);
-                            spawnPointX = spawnPointT.getX();
-                            spawnPointZ = spawnPointT.getZ();
-                            break;
-                        case ZERO_ZERO:
-                        default:
-                            spawnPointX = 0;
-                            spawnPointZ = 0;
-                            break;
-                    }
-                    List<Feature> spawnedFeatures = new ArrayList<>();
-                    for (HashMap.Entry<Feature, List<CPos>> feature : foundFeatures.entrySet()) {
-
-                        //This is a check again to make sure we are working with structures not decorators
-                        RegionStructure structure;
-                        if ((structure = (RegionStructure) feature.getKey()) instanceof RegionStructure) {
-                            for (CPos chunkPos : feature.getValue()) {
-
-                                //TODO clean this up... this is dumb I am also exhausted so whatever good enough for now
-                                if (structure.getValidDimension() == Dimension.OVERWORLD) {
-                                    if (!structure.canSpawn(chunkPos, sourceO)) continue;
-                                } else if (structure.getValidDimension() == Dimension.NETHER) {
-                                    if (!structure.canSpawn(chunkPos, sourceN)) continue;
-                                } else if (structure.getValidDimension() == Dimension.END) {
-                                    if (!structure.canSpawn(chunkPos, sourceE)) continue;
-                                } else {
-                                    //This theoretically should never be possible
-                                    continue;
-                                }
-
-                                //TODO For now this is only checking if 1 structure exists, need to bring back multi searching
-                                spawnedFeatures.add(structure);
+                        switch (model.getSpawnType()) {
+                            case APPROXIMATE:
+                                BPos spawnPoint = SpawnPoint.getApproximateSpawn(sourceO);
+                                spawnPointX = spawnPoint.getX();
+                                spawnPointZ = spawnPoint.getZ();
                                 break;
+                            case TRUESPAWN:
+                                OverworldTerrainGenerator ts = new OverworldTerrainGenerator(sourceO);
+                                BPos spawnPointT = SpawnPoint.getSpawn(ts);
+                                spawnPointX = spawnPointT.getX();
+                                spawnPointZ = spawnPointT.getZ();
+                                break;
+                            case ZERO_ZERO:
+                            default:
+                                spawnPointX = 0;
+                                spawnPointZ = 0;
+                                break;
+                        }
+                        List<Feature> spawnedFeatures = new ArrayList<>();
+                        for (HashMap.Entry<Feature, List<CPos>> feature : foundFeatures.entrySet()) {
+
+                            //This is a check again to make sure we are working with structures not decorators
+                            RegionStructure structure;
+                            if ((structure = (RegionStructure) feature.getKey()) instanceof RegionStructure) {
+                                for (CPos chunkPos : feature.getValue()) {
+
+                                    if (!structure.canSpawn(chunkPos, sourceO) && !structure.canSpawn(chunkPos, sourceN) && !structure.canSpawn(chunkPos, sourceE))
+                                        continue;
+
+                                    //TODO For now this is only checking if 1 structure exists, need to bring back multi searching
+                                    spawnedFeatures.add(structure);
+                                    break;
+                                }
                             }
                         }
-                    }
 
-                    //If we missed even one feature from our list the seed is invalid and start a new seed
-                    if (spawnedFeatures.size() != featureList.size()) {
-                        break;
-                    }
+                        //If we missed even one feature from our list the seed is invalid and start a new seed
+                        if (spawnedFeatures.size() != featureList.size()) {
+                            break;
+                        }
 
-                    ///////////// Checking Biomes ////////////////
-                    for (int x = -model.getSearchRadius() + spawnPointX; x < model.getSearchRadius() + spawnPointX; x += model.getIncrementer()) {
-                        for (int z = -model.getSearchRadius() + spawnPointZ; z < model.getSearchRadius() + spawnPointZ; z += model.getIncrementer()) {
+                        List<Biome> foundIncludedBiomes = new ArrayList<>();
+                        List<BiomeSet_Model> foundIncludedBiomeSets = new ArrayList<>();
+                        ///////////// Checking Biomes ////////////////
+                        for (int x = -model.getSearchRadius() + spawnPointX; x < model.getSearchRadius() + spawnPointX; x += model.getIncrementer()) {
+                            for (int z = -model.getSearchRadius() + spawnPointZ; z < model.getSearchRadius() + spawnPointZ; z += model.getIncrementer()) {
 
-                            //Takes all 3 world source objects (Overworld, Nether and End) and gets the biome at X and Z location for each world individually
-                            List<Biome> foundInWorlds = Arrays.asList(sourceO.getBiome(x, 0, z), sourceN.getBiome(x, 0, z), sourceE.getBiome(x, 0, z));
+                                //Takes all 3 world source objects (Overworld, Nether and End) and gets the biome at X and Z location for each world individually
+                                List<Biome> foundInWorlds = Arrays.asList(sourceO.getBiome(x, 0, z), sourceN.getBiome(x, 0, z), sourceE.getBiome(x, 0, z));
 
-                            //excludedSets Im not sure if they are even useful... it is here... not really sure how to test it
-                            //this might not be in the UI In the future
+                                //excludedSets Im not sure if they are even useful... it is here... not really sure how to test it
+                                //this might not be in the UI In the future
                             /*
                             for (BiomeSet_Model excludedSet : bsModel.getExcludedBiomeSet()) {
                                 if (excludedSet.containsAny(foundInWorlds)) {
@@ -182,37 +176,47 @@ public class Searching_Thread extends Thread implements Runnable {
                             }
                              */
 
-                            //NOTE is this perfomant? Should I find a better way?
-                            //Checking if any biome set has the biome found. If so remove it from the included list
-                            for (BiomeSet_Model includedSet : bsModel.getIncludedBiomeSet()) {
-                                if (includedSet.containsAny(foundInWorlds)) {
+                                //NOTE is this perfomant? Should I find a better way?
+                                //Checking if any biome set has the biome found. If so remove it from the included list
+                                for (BiomeSet_Model includedSet : bsModel.getIncludedBiomeSet()) {
+                                    if (includedSet.containsAny(foundInWorlds) && !foundIncludedBiomeSets.contains(includedSet)) {
 
-                                    bsModel.getIncludedBiomeSet().remove(includedSet);
-                                    break;
+                                        foundIncludedBiomeSets.add(includedSet);
+                                        break;
+                                    }
+                                }
+
+                                //Returning if we found and excluded biome
+                                if (!Collections.disjoint(foundInWorlds, bModel.getExcludedBiomes())) {
+                                    break start;
+                                }
+
+                                //Removing found biomes from included biomes
+                                //bModel.getIncludedBiomes().removeAll(foundInWorlds);
+
+                                //Todo write my own containsAny function
+                                foundInWorlds.forEach(biome -> {
+                                    if (model.getBiomeList().getIncludedBiomes().contains(biome) && !foundIncludedBiomes.contains(biome)) {
+                                        foundIncludedBiomes.add(biome);
+                                    }
+                                });
+
+
+                                //Allowed to validate if no excluded biomes were passed in
+                                if (bModel.getExcludedBiomes().isEmpty() && validateSeed(worldSeed, foundIncludedBiomes, foundIncludedBiomeSets, spawnedFeatures)) {
+                                    return;
                                 }
                             }
-
-                            //Returning if we found and excluded biome
-                            if (!Collections.disjoint(foundInWorlds, bModel.getExcludedBiomes())) {
-                                return;
-                            }
-
-                            //Removing found biomes from included biomes
-                            bModel.getIncludedBiomes().removeAll(foundInWorlds);
-
-                            //Allowed to validate if no excluded biomes were passed in
-                            if (bModel.getExcludedBiomes().isEmpty() && validateSeed(worldSeed, bModel, bsModel, spawnedFeatures)) {
-                                return;
-                            }
                         }
-                    }
 
-                    // Validate the seed after it has finished all loops
-                    validateSeed(worldSeed, bModel, bsModel, spawnedFeatures);
+                        // Validate the seed after it has finished all loops
+                        validateSeed(worldSeed, foundIncludedBiomes, foundIncludedBiomeSets, spawnedFeatures);
+                    }
                 }
             }
         }
     }
+
 
     HashMap<Boolean, HashMap<Feature, List<CPos>>> featureSearch(List<Feature> featureList, long structureSeed, ChunkRand rand) {
         HashMap<Boolean, HashMap<Feature, List<CPos>>> data = new HashMap<>();
@@ -222,7 +226,9 @@ public class Searching_Thread extends Thread implements Runnable {
         for (Feature feature : model.getFeatureList()) {
 
             RegionStructure structure;
+            //Structure strongholdData;
             // Checks if we are looking at a structure or a decorator
+
             if ((structure = (RegionStructure) feature) instanceof RegionStructure) {
                 //Get the lower and upperbound of the chunks possible
 
@@ -253,6 +259,10 @@ public class Searching_Thread extends Thread implements Runnable {
                 if (possibleChunks.isEmpty()) break;
                 foundFeatures.put(feature, possibleChunks);
             }
+
+//            if ((strongholdData = (Structure) feature) instanceof Structure) {
+//
+//            }
         }
 
         //At this point if the features we found are not the same size and the features we want to find break and go to the next seed
@@ -268,10 +278,13 @@ public class Searching_Thread extends Thread implements Runnable {
     }
 
 
-    boolean validateSeed(long seed, BiomeList_Model bModel, BiomeSetList_Model bsModel, List<Feature> foundFeatures) {
+    boolean validateSeed(long seed, List<Biome> foundIncludedBiomes, List<BiomeSet_Model> foundIncludedBiomeSets, List<Feature> foundFeatures) {
         //We need to pass in the found features to make sure, the size matches. Since we checking that before biomes, we can't rely on the featurelist being zero
         //TODO See if I can do the same thing with found features and remove it as they are found
-        if (bModel.getIncludedBiomes().isEmpty() && bsModel.getIncludedBiomeSet().isEmpty() && foundFeatures.size() == model.getFeatureList().size()) {
+        if (foundIncludedBiomes.size() == model.getBiomeList().getIncludedBiomes().size() &&
+                foundIncludedBiomeSets.size() == model.getBiomeSetList().getIncludedBiomeSet().size() &&
+                foundFeatures.size() == model.getFeatureList().size()) {
+
             //System.out.format("Found biomes %d : Model %d\n ", foundBiomes.size(), bModel.getIncludedBiomes().size());
 
             // This is only because if what you are searching will pass pretty much everytime it can go over the limit
